25148b7301c444a470271814a6fdda3b5fac0a99
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2023-02-12 18:27:29 -0600

    Pruebas para verificar Combos

---
+++ WinTestPrueba/ClienteBE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTestPrueba
+{
+    internal class ClienteBE
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+
+     
+    }
+}
